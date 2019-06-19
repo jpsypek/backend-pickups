8b54d806b971151137f2f48385ea716428e458fb
github username <github email address>
2019-06-19 17:31:01 -0600

    added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   has_many :user_events, dependent: :destroy
   has_many :users, through: :user_events
   validates :time, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :user_events
   has_many :events, through: :user_events
   has_secure_password
-  validates :email, uniqueness: :true
+  validates :email, uniqueness: true
 end
