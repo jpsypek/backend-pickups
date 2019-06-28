565f823427eced3983b20042948c6012b6a48f72
github username <github email address>
2019-06-28 10:56:52 -0400

    remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-UserEvent.destroy_all
-Event.destroy_all
-
-spike = Event.create(sport:"Spikeball", time: DateTime.parse("15th Jun 2019 18:00:00"), skill_level: "Advanced", latitude: 39.702031, longitude: -104.971130)
-soccer = Event.create(sport:"Soccer", time: DateTime.parse("14th Jun 2019 14:00:00"), skill_level: "Beginner", latitude: 39.734174, longitude: -104.966106)
-soccer = Event.create(sport:"Soccer", time: DateTime.parse("18th Jun 2019 18:00:00"), skill_level: "Advanced", latitude: 39.745726, longitude: -104.956894)
