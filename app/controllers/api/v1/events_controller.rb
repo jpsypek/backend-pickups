168026a872405b8fd88a3beef6281ae672311e7d
github username <jpsypek@gmail.com>
2019-08-26 11:00:32 -0600

    add before action to event

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::EventsController < ApplicationController
+  before_action :find_event, only: [:update, :destroy]
 
   def index
     @events = Event.all.select{|event| event.time > Time.now}
@@ -11,7 +12,6 @@ class Api::V1::EventsController < ApplicationController
   end
 
   def update
-    @event = Event.find(params[:id])
     @event.update(event_params)
     render json: @event
   end
@@ -25,6 +25,10 @@ class Api::V1::EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:sport, :time, :skill_level, :location, :latitude, :longitude, :owner)
+  end
+
+  def find_event
+    @event = Event.find(params[:id])
   end
 
 end
